ebebe5583458d2137e85d667d212cc9d5c2eb824
Andrean <88448470+QkoSad@users.noreply.github.com>
2023-05-08 16:51:12 +0000

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,27 +2,12 @@ import axios from 'axios';
 import store from '../store';
 import { LOGOUT } from '../actions/types';
 
-<<<<<<< HEAD
-// Create an instance of axios
-=======
->>>>>>> cc38df43629d64ca77f694c971a13a026b3afcfb
 const api = axios.create({
   baseURL: '/api',
   headers: {
     'Content-Type': 'application/json'
   }
 });
-<<<<<<< HEAD
-/*
-  NOTE: intercept any error responses from the api
- and check if the token is no longer valid.
- ie. Token has expired or user is no longer
- authenticated.
- logout the user if the token has expired
-*/
-=======
->>>>>>> cc38df43629d64ca77f694c971a13a026b3afcfb
-
 api.interceptors.response.use(
   (res) => res,
   (err) => {
