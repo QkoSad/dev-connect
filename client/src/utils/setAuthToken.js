bf60d78f532a4c245f900ec0d51c98c6f9b3e964
Andrean <88448470+QkoSad@users.noreply.github.com>
2023-05-08 16:51:49 +0000

    Update setAuthToken.js

--- client/src/utils/setAuthToken.js
+++ client/src/utils/setAuthToken.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import api from './api';
 
 // store our JWT in LS and set axios headers if we do have a token
@@ -10,17 +9,6 @@ const setAuthToken = (token) => {
   } else {
     delete api.defaults.headers.common['x-auth-token'];
     localStorage.removeItem('token');
-=======
-import axios from "axios";
-
-const setAuthToken = (token) => {
-  if (token) {
-    axios.defaults.headers.common["x-auth-token"] = token;
-    localStorage.setItem(('token'), token)
-  } else {
-    delete axios.defaults.common["x-auth-token"];
-    localStorage.removeItem('token')
->>>>>>> cc38df43629d64ca77f694c971a13a026b3afcfb
   }
 };
 
