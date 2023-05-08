78a88c628403da86ca8175f5ec2afec79f223956
Andrean <88448470+QkoSad@users.noreply.github.com>
2023-05-08 16:58:46 +0000

    Update Landing.js

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -5,7 +5,6 @@ import PropTypes from 'prop-types';
 
 const Landing = ({ isAuthenticated }) => {
   if (isAuthenticated) {
-<<<<<<< HEAD
     return <Navigate to="/dashboard" />;
   }
 
@@ -23,25 +22,6 @@ const Landing = ({ isAuthenticated }) => {
               Sign Up
             </Link>
             <Link to="/login" className="btn btn-light">
-=======
-    return <Navigate to='/dashboard' />;
-  }
-
-  return (
-    <section className='landing'>
-      <div className='dark-overlay'>
-        <div className='landing-inner'>
-          <h1 className='x-large'>Developer Connector</h1>
-          <p className='lead'>
-            Create a developer profile/portfolio, share posts and get help from
-            other developers
-          </p>
-          <div className='buttons'>
-            <Link to='/register' className='btn btn-primary'>
-              Sign Up
-            </Link>
-            <Link to='/login' className='btn btn-light'>
->>>>>>> cc38df43629d64ca77f694c971a13a026b3afcfb
               Login
             </Link>
           </div>
@@ -55,16 +35,9 @@ Landing.propTypes = {
   isAuthenticated: PropTypes.bool
 };
 
-<<<<<<< HEAD
+
 const mapStateToProps = (state) => ({
   isAuthenticated: state.auth.isAuthenticated
 });
 
 export default connect(mapStateToProps)(Landing);
-=======
-const mapStateToProps = state => ({
-  isAuthenticated: state.auth.isAuthenticated
-});
-
-export default connect(mapStateToProps)(Landing);
->>>>>>> cc38df43629d64ca77f694c971a13a026b3afcfb
