1e64a910cc2c63a567f48b3e1ba22211830c5869
QkoSad <malakrapon@gmail.com>
2023-06-07 10:54:19 +0300

    prepare for prod

--- server.js
+++ server.js
@@ -13,12 +13,15 @@ app.use('/api/auth', require('./routers/api/auth'))
 app.use('/api/profile', require('./routers/api/profile'))
 app.use('/api/posts', require('./routers/api/posts'))
 // Serve static assets in production
+
 if (process.env.NODE_ENV==='production'){
     app.use(express.static('client/build'));
     app.get('*',(req, res)=>[
         res.sendFile(path.resolve(__dirname, 'client', 'build','index.html'))
     ])
 }
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT,()=> console.log(`Server started on port ${PORT}`));
