da43bec8422ea3609761f0df7cd5954ae4355e30
QkoSad <malakrapon@gmail.com>
2024-09-29 00:11:08 +0300

    Removed all of the duplicate js files from the rebase for github

--- client/src/actions/alert.js
+++
@@ -1,9 +0,0 @@
-import { v4 as uuidv4 } from 'uuid';
-import {removeAlert, setAlert } from '../reducers/alert';
-
-export const createAlert = (msg, alertType, timeout = 5000) => dispatch => {
-  const id = uuidv4();
-  dispatch(setAlert({ msg, alertType, id }));
-
-  setTimeout(() => dispatch(removeAlert(id)), timeout);
-};
--- client/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {createRoot} from 'react-dom/client'
-import App from './App';
-
-// Level - 0
-//Program stars from Here. Imports and renders App.
-
-const root = createRoot(document.getElementById('root'));
-root.render(<App/>)
